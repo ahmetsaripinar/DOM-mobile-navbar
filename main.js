9e973c0b335678a461f46605ffb6974cd57a3413
ahmetsaripinar <ahmetmsaripinar@gmail.com>
2024-01-04 22:26:04 +0300

    hello git

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const menu_hide=true;
 let menu_container=document.getElementById("nav-bar-id");
 let btn = document.getElementById("icon-id");
+const menu_hide=true;
 
 function show () {
   if(menu_hide==true) {
